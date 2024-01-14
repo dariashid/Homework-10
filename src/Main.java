aab75be91f9cf306b26c3a08d56f99c78859497a
dariashid <dariashid@mail.ru>
2024-01-14 18:26:33 +0300

    homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Задача 1");
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+
+        System.out.println("Задача 2");
+        String newFullName = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + newFullName);
+
+        System.out.println("Задача 3");
+        String completeName = "Иванов Семён Семёнович";
+        completeName = completeName.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника — " + completeName);
     }
 }
